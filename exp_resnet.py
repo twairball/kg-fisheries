4b9c1ea5dd512335a259db612785b110fc7615d3
twairball <twairball@yahoo.com>
2017-03-07 23:18:22 +0800

    resnet run

--- exp_resnet.py
+++ exp_resnet.py
@@ -33,6 +33,36 @@ def train_lr():
         print("")
         print("")
 
+def train_model():
+    print("===== loading conv features =====")
+    pcm = PrecalcResnet('data/')
+    (conv_feat, conv_val_feat) = pcm.get_conv_feats()
+    
+    model = ResnetDenseModel('data/', lr=1e-3)
+    print("====== training model ======")
+    print("model: %s" % model.model_name)
+    model.train(conv_feat, conv_val_feat, nb_epoch=50)
+    return model
+
+def run_test(m):
+    print("====== load test conv feats ======")
+    tm = PrecalcResnetTest('data/')
+    conv_test_feat = tm.get_conv_feats()
+
+    print("====== run test ======")
+    preds = m.test(conv_test_feat)
+
+def run_submit(m):
+    print("======= making submission ========")
+    preds = load_array(m.preds_path)
+    test_batch = get_batches('data/test/')
+    submit_path = 'submits/' + m.model_name + '_subm.gz'
+    submit(preds, test_batch, submit_path)
+
+    print("======= pushing to kaggle ========")
+    push_to_kaggle(submit_path)
+
 if __name__ == "__main__":
-    # precalc()
-    train_lr()
+    m = train_model()
+    run_test(m)
+    run_submit(m)

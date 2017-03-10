45ef0c10ad69f7e7b3236a6684c8ac3b188d007d
twairball <twairball@yahoo.com>
2017-03-10 12:15:40 +0800

    ensemble predictions save and submit

--- experiments.py
+++ experiments.py
@@ -1,6 +1,7 @@
 from base_model import BaseModel
 from kaggle import *
 from setup import *
+from utils import save_array
 
 ##
 ## main scripts
@@ -37,18 +38,47 @@ def train_dense_nodes():
 def train_ensemble():
     nb_models = 5 # train 5 ensemble models
     models = []
+    nb_test_samples = 1000
+    nb_classes = 8
+    preds = np.zeros((nb_test_samples, nb_classes))
+
     for run in range(nb_models):
+        print("====== Ensemble model: %d ======" % run)
         m = BaseModel('data/', dense_nodes=512)
+        model_prefix = "da_r%d_" % run 
+        m.model_name = model_prefix + m.model_name
 
         print("====== training model ======")
         m = BaseModel('data/', dense_nodes=512)
         m.train(nb_epoch = 20, use_da=True)
 
         print("====== running test ======")
-        preds, test_batches = m.test(use_da=True)
-        
+        _preds, _test_batches = m.test(use_da=True)
+
+        # append predictions
+        preds = preds + _preds
+
+        # append model 
         models = models + [m]
-    return models
+    
+    # average
+    preds /= nb_models
+    save_array('data/results/ensemble_dn512_ep20_da_preds.h5', preds)
+
+    return preds, models
     
 if __name__ == "__main__":
-    train_ensemble()
+    preds, models = train_ensemble()
+
+    del models 
+
+    # get test batch from any model 
+    test_batches = BaseModel('data/').create_test_batches()    
+
+    print("======= making submission ========")
+    submits_path = 'submits/ens_dn512_ep20_da_subm.gz'
+    submit(preds, test_batches, submits_path)
+
+    print("======= pushing to kaggle ========")
+    push_to_kaggle(submits_path)
+    
